20985fd47fd214ab515b7afd41e57ee5ad34ac05
DonOutcast <sham1996@yandex.ru>
2023-04-25 11:21:10 +0300

    paused at page 46

--- chapter_01/mysite/blog/models.py
+++ chapter_01/mysite/blog/models.py
@@ -3,12 +3,17 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']
