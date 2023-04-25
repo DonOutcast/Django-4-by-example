e88d25507c71a1e7010f17d2b1991fa4a5bba34d
DonOutcast <sham1996@yandex.ru>
2023-04-25 12:40:49 +0300

    paused at page 50

--- chapter_01/mysite/blog/models.py
+++ chapter_01/mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -9,8 +10,9 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone)
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
