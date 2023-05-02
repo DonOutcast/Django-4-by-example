05354d6a4591d9710106f6080e62866d42b7e672
DonOutcast <sham1996@yandex.ru>
2023-05-02 11:24:22 +0300

    started chapter 02

---
+++ chapter_02/mysite/blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class PbulishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PbulishedManager()
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
