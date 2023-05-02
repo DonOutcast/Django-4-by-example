1958956e147a69a494fcb3a19f743e2a2faf2544
DonOutcast <sham1996@yandex.ru>
2023-05-02 11:23:10 +0300

    stoped at page 72

--- chapter_01/mysite/blog/views.py
+++ chapter_01/mysite/blog/views.py
@@ -6,11 +6,11 @@ from .models import Post
 
 def post_list(request):
     posts = Post.published.all()
-    return (request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, )
+    post = get_object_or_404(Post, id=id)
     return render(request, 'blog/post/detail.html', {'post': post})
 
 
