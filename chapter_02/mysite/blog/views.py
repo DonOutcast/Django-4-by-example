90759ac1b1d57a0c358a840117966beec36f4601
DonOutcast <sham1996@yandex.ru>
2023-05-15 10:52:39 +0300

    paused at a page 90

--- chapter_02/mysite/blog/views.py
+++ chapter_02/mysite/blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+class PostListView(ListView):
+
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 def post_list(request):
     post_list = Post.published.all()
