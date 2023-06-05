5215701d945e598390c2fd5718d59ba837f4d753
DonOutcast <sham1996@yandex.ru>
2023-06-05 10:51:47 +0300

    stoped at page 124

--- chapter_03/mysite/blog/templatetags/blog_tags.py
+++ chapter_03/mysite/blog/templatetags/blog_tags.py
@@ -1,7 +1,20 @@
 from django import template
 from blog.models import Post
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
+
+
+@register.filter(name="markdown")
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
 
 
 @register.simple_tag
--- chapter_03/mysite/mysite/settings.py
+++ chapter_03/mysite/mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
