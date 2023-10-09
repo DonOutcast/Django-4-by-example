6d7fd502a1ab18e55c4df977416988235d246be4
DonOutcast <sham1996@yandex.ru>
2023-10-09 15:43:09 +0300

    test 7

--- simple.py
+++ simple.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import get_user_model
 import asyncio
-
-import os
-from .chapter_01.mysite.blog.models import Post
 import functools
+import os
+
+from django.contrib.auth import get_user_model
+
+from .chapter_01.mysite.blog.models import Post
