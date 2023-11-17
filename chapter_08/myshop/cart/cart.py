38a6bb0af5e4053980c2301ed2f2a06a62a01a73
DonOutcast <sham1996@yandex.ru>
2023-11-17 16:23:35 +0300

    stoped at page 432

--- chapter_08/myshop/cart/cart.py
+++ chapter_08/myshop/cart/cart.py
@@ -48,7 +48,7 @@ class Cart:
         return sum(item["quantity"] for item in self.cart.values())
 
     def get_total_price(self):
-        return sum(Decimal(item["price"] * item["quantity"]) for item in self.cart.values())
+        return sum(Decimal(float(item["price"]) * item["quantity"]) for item in self.cart.values())
 
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
--- simple.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-import functools
-import os
-
-from django.contrib.auth import get_user_model
-
-from .chapter_01.mysite.blog.models import Post
